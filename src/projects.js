a926dedcf0712a1f6ac8649aae77c0f280279d4f
lkk6057 <30610351+lkk6057@users.noreply.github.com>
2022-03-28 03:41:56 -0400

    .

--- src/projects.js
+++ src/projects.js
@@ -1,19 +1,31 @@
 function isInViewport(element) {
     const yPos = element.getBoundingClientRect().y;
-    return yPos<window.innerHeight;
+    return yPos < window.innerHeight && yPos > -element.offsetHeight;
 }
 
+
 var projects = document.getElementsByName("project");
-window.addEventListener("scroll",scroll);
-function scroll(e){
-    for(var i = 0;i<projects.length;i++){
+updateProjectNodes();
+
+$( document ).ready(function() {
+   updateProjectNodes();
+});
+
+window.addEventListener("scroll", scroll);
+
+function scroll(e) {
+    updateProjectNodes();
+}
+
+function updateProjectNodes() {
+    for (var i = 0; i < projects.length; i++) {
         var ele = projects[i];
         var inview = isInViewport(ele);
-        if(inview){
-           ele.className = "card left slideInLeft";
-           }
-        else{
-          ele.className = "card left";  
+                    var even = false;
+        if (inview) {
+            ele.className = "card slideInLeft";
+        } else {
+            ele.className = "card left"
         }
     }
 }
